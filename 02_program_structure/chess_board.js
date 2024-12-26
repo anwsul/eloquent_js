0def545552e69677e3d51cc8de466c1913799a5a
anwsul <ansu.khiyar@gmail.com>
2024-12-26 09:02:17 +0300

    Program structure: loops and conditionals

---
+++ 02_program_structure/chess_board.js
@@ -0,0 +1,10 @@
+even = " # # # #"
+odd = "# # # # "
+size = 8
+
+for (let i = 0; i < size; i++) {
+  if (i % 2 == 0)
+    console.log(even)
+  else
+    console.log(odd)
+}
---
+++ 02_program_structure/looping_a_triangle.js
@@ -0,0 +1,5 @@
+string = "#";
+for (let i = 0; i < 7; i++) {
+  console.log(string);
+  string += "#";
+}
